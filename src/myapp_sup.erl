7a93e89540c95fb284ca372cabc87fdb7edf0b58
vk <v.katsuba.dev@gmail.com>
2026-04-15 19:09:18 +0300

    improvements

--- src/myapp_sup.erl
+++ src/myapp_sup.erl
@@ -12,14 +12,14 @@ init(_Args) ->
     CountParallelRequests = 100,
     GunMaxActiveRequests = 50,
     HttpcProfiles = [profile_1, profile_2, profile_3, profile_4, profile_5],
-    HttpcLimitPerProfile = 50,
+    HttpcLimitPerProfile = 10,
     RequestBodyBytes = 1000000,
     EnableHttpcDebugTrace = false,
     TraceLimit = 1000,
     Host = "https://caddy.localhost",
     Counter = counters:new(2, []),
     ok = myapp_request_body:init(),
-    ok = maybe_start_httpc_profiles(Client, HttpcProfiles),
+    ok = maybe_start_httpc_profiles(Client, HttpcProfiles, Host, RequestBodyBytes),
 
     OptionalChildSpecs =
         (case Client =:= gun of
@@ -49,16 +49,47 @@ init(_Args) ->
       #{id => stats, start => {gen_server, start_link, [{local, stats}, myapp_stats, [Counter, Client, RequestBodyBytes], []]}}
     | OptionalChildSpecs ++ ChildSpecs]}}.
 
-maybe_start_httpc_profiles(httpc, Profiles) ->
+maybe_start_httpc_profiles(httpc, Profiles, Host, RequestBodyBytes) ->
     lists:foreach(
       fun(Profile) ->
           case inets:start(httpc, [{profile, Profile}]) of
               {ok, _Pid} -> ok;
               {error, {already_started, _Pid}} -> ok;
               ok -> ok
-          end
+          end,
+          maybe_set_httpc_profile_options(Profile),
+          maybe_warm_httpc_profile(Profile, Host, RequestBodyBytes)
       end,
       Profiles),
     ok;
-maybe_start_httpc_profiles(_, _) ->
+maybe_start_httpc_profiles(_, _, _, _) ->
+    ok.
+
+maybe_set_httpc_profile_options(Profile) ->
+    _ = catch httpc:set_options(
+        [
+            {max_sessions, 50},
+            {max_keep_alive_length, 1000},
+            {keep_alive_timeout, 120000}
+        ],
+        Profile),
+    ok.
+
+maybe_warm_httpc_profile(Profile, Host, RequestBodyBytes) ->
+    WarmBody = myapp_request_body:get(RequestBodyBytes),
+    WarmReq = {post,
+               {Host,
+                [{"X-Request-Id", <<"warmup">>}],
+                "application/x-www-form-urlencoded",
+                iolist_to_binary(WarmBody)}},
+    lists:foreach(
+      fun(_) ->
+          _ = catch httpc:request(
+              element(1, WarmReq),
+              element(2, WarmReq),
+              [{ssl, [{verify, verify_none}]}],
+              [],
+              Profile)
+      end,
+      lists:seq(1, 2)),
     ok.
